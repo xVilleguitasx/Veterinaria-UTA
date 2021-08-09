8c5078d7ab449495b6fbbf24fd419f829693f0ed
Alex Villegas <av058554@gmail.com>
2021-08-09 18:59:58 -0500

    Interfaz principal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HistoriaclinicaComponent } from './componentes/historiaclinica/historiaclinica.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { LoginComponent } from './pages/login/login.component';
 
 const routes: Routes = [
-  {path:'', pathMatch:'full', component:HistoriaclinicaComponent},
+  {path:'', pathMatch:'full', component:LoginComponent},
+  {path:'inicio', component:InicioComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HistoriaclinicaComponent } from './componentes/historiaclinica/historiaclinica.component';
+import { LoginComponent } from './pages/login/login.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HistoriaclinicaComponent
+    HistoriaclinicaComponent,
+    LoginComponent,
+    InicioComponent
   ],
   imports: [
     BrowserModule,
