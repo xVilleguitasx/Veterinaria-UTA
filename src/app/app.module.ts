e312b24d1b2de5921b6fcc26d51e1d284b3f683d
Alex Villegas <av058554@gmail.com>
2021-08-09 19:31:06 -0500

    Historias clinicas  v1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HistoriaclinicaComponent } from './componentes/historiaclinica/historiaclinica.component';
 import { LoginComponent } from './pages/login/login.component';
 import { InicioComponent } from './pages/inicio/inicio.component';
+import { HistoriaClinicaComponent } from './pages/historia-clinica/historia-clinica.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HistoriaclinicaComponent,
     LoginComponent,
-    InicioComponent
+    InicioComponent,
+    HistoriaClinicaComponent
   ],
   imports: [
     BrowserModule,
